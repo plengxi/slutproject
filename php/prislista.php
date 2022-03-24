193189e829825a1fbb0615014546827309aa79a2
DVDBOSSS <hnilssonberglind@gmail.com>
2022-03-24 10:56:06 +0100

    ommig delen + css

--- php/prislista.php
+++ php/prislista.php
@@ -1,17 +1,33 @@
-<img src="../svg/goldband.svg" alt="goldband">
-<div class="prislista">
-<p>Helkroppsmassage</p>
-<p>Fotmassage<br>(Reflexology)</p>
-<p>Oljemassage<br>(Bara ryggen)</p>
-<p>Ansiktsbehandling</p>
 
-<p>400 kr/60 min</p>
-<p>300 kr/45 min</p>
-<p>300 kr/45 min</p>
-<p>300 kr/45 min</p>
+<link rel="stylesheet" href="../css/prislista.min.css">
+
+<div class="goldbandContainer">
+  <img id="goldband" src="../svg/goldband.svg" alt="goldband">
 </div>
 
-<img src="../img/giftcard.jpg" alt="presentkort">
+<div class="prislista">
+
+  <div class="leftSide">
+
+    <p>Helkroppsmassage</p>
+    <p>Fotmassage<br>(Reflexology)</p>
+    <p>Oljemassage<br>(Bara ryggen)</p>
+    <p>Ansiktsbehandling</p>
+
+  </div>
+
+<div class="rightSide">
+
+  <p>400 kr/60 min</p>
+  <p>300 kr/45 min</p>
+  <p>300 kr/45 min</p>
+  <p>300 kr/45 min</p>
+
+</div>
+
+</div>
+
+<!--<img src="../img/giftcard.jpg" alt="presentkort">-->
 <p>Köp presentkort till din nära och kära via SMS: 070 573 0330</p>
 
 <h1>Boka tid</h1>
