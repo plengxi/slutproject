6fa35fe095f851641614b8e2cc42289fc667cb08
DVDBOSSS <hnilssonberglind@gmail.com>
2022-03-28 09:15:18 +0200

    Abdullah förstör våran kod

--- php/prislista.php
+++ php/prislista.php
@@ -5,30 +5,61 @@
   <img id="goldband" src="../svg/goldband.svg" alt="goldband">
 </div>
 
-<div class="prislista">
 
-  <div class="leftSide">
+  <div class="prislista">
+    <h1 class="headline" style="color:white;"><span style="color:#F5CB3C; font-size: 3.438rem; ">P</span>rislista</h1>
 
-    <p>Helkroppsmassage</p>
-    <p>Fotmassage<br>(Reflexology)</p>
-    <p>Oljemassage<br>(Bara ryggen)</p>
-    <p>Ansiktsbehandling</p>
+    <div class="lowline2">
+
+      <div style="background-color: #F4CB3C; " class="line"></div>
+
+    </div>
+  </div>
+
+<div class="pris">
+
+    <p class="move">Helkroppsmassage</p>
+      <p>400 kr/60 min</p>
+    <p class="move">Fotmassage<br>(Reflexology)</p>
+      <p>300 kr/45 min</p>
+    <p class="move">Oljemassage<br>(Bara ryggen)</p>
+      <p>300 kr/45 min</p>
+    <p class="move">Ansiktsbehandling</p>
+      <p>300 kr/45 min</p>
 
   </div>
 
-<div class="rightSide">
+  <div class="bg">
+    <img src="../svg/prislistabg2.svg" alt="">
+  </div>
 
-  <p>400 kr/60 min</p>
-  <p>300 kr/45 min</p>
-  <p>300 kr/45 min</p>
-  <p>300 kr/45 min</p>
+<div class="giftcontainer">
+
+  <img id="presentkort" src="../img/giftcard_11zon.jpg" alt="presentkort">
 
 </div>
 
+<div class="nummbercontainer">
+
+  <p id="nummber"><span style="color:#F5CB3C; ">Köp presentkort</span> till din nära <br> och kära via SMS: <span style="color:#F5CB3C; ">070 573 0330</span> </p>
+
 </div>
 
-<!--<img src="../img/giftcard.jpg" alt="presentkort">-->
-<p>Köp presentkort till din nära och kära via SMS: 070 573 0330</p>
+<div class="bokacontainer">
 
-<h1>Boka tid</h1>
-<p>SMS</p><p>070 573 0330</p>
+  <h1 id="boka"><span style="color: #F5CB3C; font-size: 3.438rem; ">B</span>oka tid</h1>
+
+  <div class="lowline">
+
+    <div style="background-color: white; " class="line lineup"></div>
+
+  </div>
+
+  <div class="sc">
+
+    <p id="sms">SMS </p>
+    <p id="tel">070 573 0330 </p>
+
+  </div>
+
+</div>
